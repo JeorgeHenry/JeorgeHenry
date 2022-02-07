fb95e0133846919a71e0d659a3212f58e923c688
Greenkitty99 <musicwillow123@gmail.com>
2022-02-06 19:52:47 -0500

    hhh
    any

---
+++ data.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'greenkitty99',
+    short: "gk99",
+    realname: "johnhenry",
+    description: `I am a human. I like to code. Hamburger`,
+    url: 'https://replname.username.repl.co',
+    avatar: "/me.png"
+}
